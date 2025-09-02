c5c491100e6e750b84c31b30b61402998ca3d44f
timothiousgill <timothious.gill@ili.digital>
2025-09-02 11:20:23 +0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 // App.tsx
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import ProductDetails from "./pages/ProductDetails";
+import Profile from "./pages/Profile";
+import OrderHistory from "./pages/OrderHistory";
+import Settings from "./pages/Settings";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} /> 
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} /> 
+        <Route path="/profile" element={<Profile />} /> 
+        <Route path="/history" element={<OrderHistory />} />
+        <Route path="/settings" element={<Settings />} /> 
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* fallback */}
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,12 @@
+// main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
-import About from "./pages/About.tsx";
-import Contact from "./pages/Contact.tsx";
-import Shop from "./pages/Shop.tsx";
-import Cart from "./pages/Cart.tsx";
-import Checkout from "./pages/Checkout.tsx"; 
-import ProductDetails from "./pages/ProductDetails.tsx";
-import Profile from "./pages/Profile.tsx"; 
-import OrderHistory from "./pages/OrderHistory.tsx"; 
-import Settings from "./pages/Settings.tsx";
-import Navbar from "./components/Navbar.tsx";
-import { CartProvider } from "./components/context/CartContext.tsx";
-import { AuthProvider } from "./components/context/AuthContext.tsx"; 
-
-import { Provider } from './components/ui/provider.tsx'
+import App from './App'
+import { CartProvider } from "./components/context/CartContext";
+import { AuthProvider } from "./components/context/AuthContext"; 
+import { Provider } from './components/ui/provider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -24,19 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <CartProvider>
           <BrowserRouter>
-            <Navbar />
-            <Routes>
-              <Route path="/" index element={<App />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/product/:id" element={<ProductDetails />} /> 
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/checkout" element={<Checkout />} /> 
-              <Route path="/profile" element={<Profile />} /> 
-              <Route path="/history" element={<OrderHistory />} />
-              <Route path="/settings" element={<Settings />} /> 
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+            <App />
           </BrowserRouter>
         </CartProvider>
       </AuthProvider>
